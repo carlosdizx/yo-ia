99ae0ec719f74679d426dd433ed972147e25b80a
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-07-26 11:14:20 -0500

    :sparkles: update messages for chat

--- src/componets/chat.py
+++ src/componets/chat.py
@@ -2,19 +2,27 @@ def chat(st):
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
-    prompt = (st.chat_input
-              (key="chat_input",
-               placeholder="Aquí puedes preguntarme cualquier cosa sobre mi creador ♥️",
-               max_chars=300)
-              )
+    messages = {
+        "es": {
+            "placeholder": "Aquí puedes preguntarme cualquier cosa sobre mi creador ♥️",
+            "response": "¡Gracias por tu mensaje! ¿En qué más puedo ayudarte hoy?"
+        },
+        "en": {
+            "placeholder": "Here you can ask me anything about my creator ♥️",
+            "response": "Thanks for your message! How else can I help you today?"
+        }
+    }
+
+    current_language = st.session_state.language
+    text = messages[current_language]
+
+    prompt = st.chat_input(key="chat_input", placeholder=text["placeholder"], max_chars=300)
 
     if prompt:
         st.session_state.messages.append({"role": "user", "content": prompt})
-        st.chat_message("user").markdown(prompt)
 
-        response = "¡Gracias por tu mensaje! ¿En qué más puedo ayudarte hoy?"
+        response = text["response"]
         st.session_state.messages.append({"role": "ai", "content": response})
-        st.chat_message("ai").info(response)
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):

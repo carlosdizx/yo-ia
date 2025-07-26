de09d6a85117c8981467fbd46db3fd799161454a
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-07-26 00:34:33 -0500

    :sparkles: update logic for banner

--- src/componets/banner.py
+++ src/componets/banner.py
@@ -5,15 +5,20 @@ def banner(st):
         Estoy aquí para responder cualquier pregunta sobre mi creador y proporcionar detalles precisos sobre su vida.
     """)
 
-    # Mostrar el mensaje de la IA con opciones como botones
-    st.chat_message("ai").markdown("""
-        Aquí tienes algunas cosas sobre las que puedes preguntarme:
-        - ¿Quién es Carlos Ernesto Díaz Basante?
-        - Su profesión y experiencia
-        - Áreas de interés
-        - Gustos y hobbies
-        - Descargar su CV
-        - Temas de interés: centros de la casa, proyectos, y más.
-        
-        Pídeme cualquier cosa que quieras saber sobre **Ernesto Díaz**.
-    """)
+    if st.button("¿Quién es Carlos Ernesto Díaz Basante?"):
+        st.session_state.chat_input = "¿Quién es Carlos Ernesto Díaz Basante?"
+
+    if st.button("Su profesión y experiencia"):
+        st.session_state.chat_input = "Su profesión y experiencia"
+    if st.button("Áreas de interés"):
+        st.session_state.chat_input = "Áreas de interés"
+    if st.button("Gustos y hobbies"):
+        st.session_state.chat_input = "Gustos y hobbies"
+    if st.button("Descargar su CV"):
+        st.session_state.chat_input = "Descargar su CV"
+    if st.button("Temas de interés (centros de la casa, proyectos, y mucho más)"):
+        st.session_state.chat_input = "Temas de interés (centros de la casa, proyectos, y mucho más)"
+
+    return st.chat_input(key="chat_input",
+                         placeholder="Escribe aquí tu mensaje...",
+                         max_chars=1000)

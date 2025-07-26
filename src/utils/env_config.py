1ffc08555e29fd86057441e1ca43227ad83796db
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-07-26 18:52:46 -0500

    :sparkles: remove logic for generate .txt and usage deep seek

--- src/utils/env_config.py
+++ src/utils/env_config.py
@@ -5,17 +5,15 @@ from dotenv import load_dotenv
 def load_config() -> dict:
     load_dotenv()
 
-    required_env_vars = ["GEMINI_API_KEY"]
+    required_env_vars = ["GEMINI_API_KEY", "DEEP_SEEK_API_KEY"]
 
     missing_vars = [var for var in required_env_vars if not os.getenv(var)]
 
     if missing_vars:
         error_message = f"ERROR: Faltan las siguientes variables de entorno: {', '.join(missing_vars)}"
 
-        with open("missing_env_vars.txt", "w", encoding="utf-8") as file:
-            file.write(error_message)
-
         raise EnvironmentError(error_message)
     return {
         "GEMINI_API_KEY": os.getenv("GEMINI_API_KEY"),
+        "DEEP_SEEK_API_KEY": os.getenv("DEEP_SEEK_API_KEY"),
     }

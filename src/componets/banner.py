6e8091a40d7e5c2a51aa720379743b0ee5f37377
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-07-26 11:11:45 -0500

    :sparkles: update messages for banner

--- src/componets/banner.py
+++ src/componets/banner.py
@@ -1,25 +1,46 @@
 def banner(st):
-    st.title("Chatbot Gemini - YO-IA")
-    st.chat_message("ai").info("""
-        ¡Hola! Soy **YO-IA**, asistente personal diseñado por **Carlos Ernesto Díaz Basante**.
-        Estoy aquí para responder cualquier pregunta sobre mi creador y proporcionar detalles precisos sobre su vida.
-    """)
+    messages = {
+        "es": {
+            "title": "IM-IA",
+            "info": "¡Hola! Soy **YO-IA**, asistente personal diseñado por **Carlos Ernesto Díaz Basante**. Estoy "
+                    "aquí para responder cualquier pregunta sobre mi creador y proporcionar detalles precisos sobre "
+                    "su vida.",
+            "questions": [
+                "¿Quién es Carlos Ernesto Díaz Basante?",
+                "Su profesión y experiencia",
+                "Áreas de interés",
+                "Gustos y hobbies",
+                "Descargar su CV",
+                "Temas de interés (centros de la casa, proyectos, y mucho más)"
+            ],
+            "ai_info": "Puedes hacerme preguntas sobre mi creador, su profesión y experiencia, temas de interés, "
+                       "etc. También puedes preguntarme sobre sus gustos y hobbies, o descargar su CV en formato PDF."
+        },
+        "en": {
+            "title": "YO-IA",
+            "info": "Hello! I am **YO-IA**, a personal assistant designed by **Carlos Ernesto Díaz Basante**. I am "
+                    "here to answer any questions about my creator and provide detailed information about his life.",
+            "questions": [
+                "Who is Carlos Ernesto Díaz Basante?",
+                "His profession and experience",
+                "Areas of interest",
+                "Likes and hobbies",
+                "Download his CV",
+                "Topics of interest (centers of the house, projects, and more)"
+            ],
+            "ai_info": "You can ask me about my creator, his profession and experience, areas of interest, etc. You "
+                       "can also ask about his likes and hobbies, or download his CV in PDF format."
+        }
+    }
 
-    if st.button("¿Quién es Carlos Ernesto Díaz Basante?"):
-        st.session_state.chat_input = "¿Quién es Carlos Ernesto Díaz Basante?"
+    current_language = st.session_state.language
+    text = messages[current_language]
 
-    if st.button("Su profesión y experiencia"):
-        st.session_state.chat_input = "Su profesión y experiencia"
-    if st.button("Áreas de interés"):
-        st.session_state.chat_input = "Áreas de interés"
-    if st.button("Gustos y hobbies"):
-        st.session_state.chat_input = "Gustos y hobbies"
-    if st.button("Descargar su CV"):
-        st.session_state.chat_input = "Descargar su CV"
-    if st.button("Temas de interés (centros de la casa, proyectos, y mucho más)"):
-        st.session_state.chat_input = "Temas de interés (centros de la casa, proyectos, y mucho más)"
+    st.title(text["title"])
+    st.chat_message("ai").info(text["info"])
 
-    (st.chat_message('ai')
-     .info("""Puedes hacerme preguntas sobre mi creador, su profesión y experiencia, temas de interés, etc.
-     También puedes preguntarme sobre sus gustos y hobbies, o descargar su CV en formato PDF.
-     """))
+    for question in text["questions"]:
+        if st.button(question):
+            st.session_state.chat_input = question
+
+    st.chat_message("ai").info(text["ai_info"])

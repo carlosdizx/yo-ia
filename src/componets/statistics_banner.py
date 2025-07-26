3c19e269b56b4eb8433cacb54252c907ce2f89b2
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-07-26 12:51:43 -0500

    :sparkles: add number for letters for user and AI

--- src/componets/statistics_banner.py
+++ src/componets/statistics_banner.py
@@ -5,6 +5,8 @@ def statistics_banner(st):
                 "title": "Estadísticas del chat",
                 "subtitle": "Mensajes enviados",
                 "subtitle2": "Cantidad de letras enviadas",
+                "subtitle3": "Cantidad de letras enviadas por el usuario",
+                "subtitle4": "Cantidad de letras enviadas por la IA",
             }
         },
         "en": {
@@ -12,6 +14,8 @@ def statistics_banner(st):
                 "title": "Chat Statistics",
                 "subtitle": "Messages sent",
                 "subtitle2": "Number of letters sent",
+                "subtitle3": "Number of letters sent by the user",
+                "subtitle4": "Number of letters sent by the AI",
             }
 
         }
@@ -22,6 +26,10 @@ def statistics_banner(st):
     if "messages" in st.session_state:
         num_messages = len(st.session_state.messages)
         num_letters = sum(len(message["content"]) for message in st.session_state.messages)
+        num_letters_user = sum(len(message["content"]) for message in st.session_state.messages if message["role"] == "user")
+        num_letters_ai = sum(len(message["content"]) for message in st.session_state.messages if message["role"] == "ai")
         st.sidebar.markdown(f"### {cha_messages['title']}")
         st.sidebar.markdown(f"{cha_messages['subtitle']}: {num_messages}")
         st.sidebar.markdown(f"{cha_messages['subtitle2']}: {num_letters}")
+        st.sidebar.markdown(f"{cha_messages['subtitle3']}: {num_letters_user}")
+        st.sidebar.markdown(f"{cha_messages['subtitle4']}: {num_letters_ai}")

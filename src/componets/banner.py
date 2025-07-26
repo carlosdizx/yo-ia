9f16147955d5b9d6f8425e5a94bafff62f738fff
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-07-25 23:52:01 -0500

    :sparkles: create banner

---
+++ src/componets/banner.py
@@ -0,0 +1,13 @@
+def banner(st):
+    st.title("Chatbot Gemini - YO-IA")
+    st.write("""
+        ¡Hola! Soy **YO-IA**, asistente personal diseñado por Carlos Ernesto Díaz Basante.
+        Estoy aquí para responder cualquier pregunta sobre mi creador y proporcionar detalles precisos sobre su vida.
+    """)
+
+    st.button("¿Quién es Carlos Ernesto Díaz Basante?")
+    st.button("Su profesión y experiencia")
+    st.button("Áreas de interés")
+    st.button("Gustos y hobbies")
+    st.button("Descargar su CV")
+    st.button("Temas de interés (centros de la casa, proyectos, y mucho más)")

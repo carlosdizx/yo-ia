1beada45f0efcefc74c039d51020d60d78793d8d
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-07-27 12:10:22 -0500

    :fire: remove unnecessary file an logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,8 @@ from src.componets.banner import banner
 from src.componets.chat import chat
 from src.componets.sidebar import sidebar
 from src.componets.statistics_banner import statistics_banner
-from src.utils.env_config import load_config
 
 sidebar(st)
 banner(st)
 chat(st)
 statistics_banner(st)
-
-load_config()

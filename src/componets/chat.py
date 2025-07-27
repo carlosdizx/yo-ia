301bf48a7d26150c6c66031beb0697243dc09e75
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-07-27 14:44:48 -0500

    :sparkles: integrate ai at chat

--- src/componets/chat.py
+++ src/componets/chat.py
@@ -1,15 +1,21 @@
-def chat(st):
+import streamlit as sst
+from src.services.gemini_service import GeminiService
+
+
+def chat(st: sst, client: GeminiService):
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
     messages = {
         "es": {
             "placeholder": "Aquí puedes preguntarme cualquier cosa sobre mi creador ♥️",
-            "response": "¡Gracias por tu mensaje! ¿En qué más puedo ayudarte hoy?"
+            "thinking": "Pensando...",
+            "error": "Ocurrió un error al generar la respuesta."
         },
         "en": {
             "placeholder": "Here you can ask me anything about my creator ♥️",
-            "response": "Thanks for your message! How else can I help you today?"
+            "thinking": "Thinking...",
+            "error": "An error occurred while generating the response."
         }
     }
 
@@ -21,12 +27,11 @@ def chat(st):
     if prompt:
         st.session_state.messages.append({"role": "user", "content": prompt})
 
-        response = text["response"]
+        with st.spinner(text["thinking"]):
+            response = client.get_response(prompt)
+
         st.session_state.messages.append({"role": "ai", "content": response})
 
-    def message_generator():
-        yield from st.session_state.messages
-
-    for message in message_generator():
+    for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,14 @@ from src.componets.banner import banner
 from src.componets.chat import chat
 from src.componets.sidebar import sidebar
 from src.componets.statistics_banner import statistics_banner
+from src.services.gemini_service import GeminiService
+
+
+st.session_state.language = "es"
+
+client = GeminiService(st.session_state.language)
 
 sidebar(st)
 banner(st)
-chat(st)
+chat(st, client)
 statistics_banner(st)
